50d8c03c961da91c6248efb95aeef3ff8fdd4e79
maatreef <maatreef33@gmail.com>
2023-01-07 09:54:19 -0300

    [FIXED] Fixed Listado

--- src/pages/Listado.tsx
+++ src/pages/Listado.tsx
@@ -1,4 +1,4 @@
-import react, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 // Importando librerías de Bootstrap
 import Card from 'react-bootstrap/card';
 import ListGroup from 'react-bootstrap/ListGroup';
