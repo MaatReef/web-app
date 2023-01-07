12ca0425be98d010480f86a7854d0cd3dce96b17
maatreef <maatreef33@gmail.com>
2023-01-07 09:22:39 -0300

    [ADD] Controllers and Views

--- src/pages/Listado.tsx
+++ src/pages/Listado.tsx
@@ -1,65 +1,51 @@
-import react from "react";
+import react, {useEffect, useState} from "react";
 import Card from 'react-bootstrap/card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { getPokemons } from "../controller/getPokemon";
+import { Pokemon } from "../models/pokemon.m"; 
+
+
+// El metodo slice() devuelve una copia de una parte del array dentro de un nuevo array empezando por inicio hasta fin (fin no incluido). El array original no se modificará. 
+
 const Listado = () => {
+    // Obtenemos la data de acuerdo al formato del modelo
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+    // Llamamos al controller y los obtenemos a todos
+    useEffect(() => {
+        const ObtenerTodos = async() => {
+            const allPokemons = await getPokemons();
+            setPokemons(allPokemons);
+        };
+        ObtenerTodos();
+    });
+
     return(
         <>
             <h1>Pokemon</h1>
             <div className="content-wrap">
                 <div className="content">
                     <div className="row gap-3">
-                        <Card className="mx-auto" style={{ width: '18rem' }}>
-                        <Card.Header>Tipo: Fantasma</Card.Header>
-                        <Card.Img width="80" height="100" variant="top" src="https://img.pokemondb.net/sprites/black-white/anim/normal/gengar.gif" className="d-block mx-auto w-50"/>
-                        <Card.Body>
-                            <Card.Title className="text-center">Gengar</Card.Title>
-                            <ListGroup variant="flush">
-                                <ListGroup.Item>Hp: ##</ListGroup.Item>
-                                <ListGroup.Item>Ataque: ##</ListGroup.Item>
-                                <ListGroup.Item>Defensa: ##</ListGroup.Item>
-                                <ListGroup.Item>E.Ataque: ##</ListGroup.Item>
-                                <ListGroup.Item>E.Defensa: ##</ListGroup.Item>
-                                <ListGroup.Item>Velocidad: ##</ListGroup.Item>
-                            </ListGroup>
-                        </Card.Body>
-                        </Card>
 
-                        
-                        <Card className="mx-auto" style={{ width: '18rem' }}>
-                        <Card.Header>Tipo: Fantasma</Card.Header>
-                        <Card.Img width="80" height="100" variant="top" src="https://img.pokemondb.net/sprites/black-white/anim/normal/gengar.gif" className="d-block mx-auto w-50"/>
-                        <Card.Body>
-                            <Card.Title className="text-center">Gengar</Card.Title>
-                            <ListGroup variant="flush">
-                                <ListGroup.Item>Hp: ##</ListGroup.Item>
-                                <ListGroup.Item>Ataque: ##</ListGroup.Item>
-                                <ListGroup.Item>Defensa: ##</ListGroup.Item>
-                                <ListGroup.Item>E.Ataque: ##</ListGroup.Item>
-                                <ListGroup.Item>E.Defensa: ##</ListGroup.Item>
-                                <ListGroup.Item>Velocidad: ##</ListGroup.Item>
-                            </ListGroup>
-                        </Card.Body>
-                        </Card>
-
-                        <Card className="mx-auto" style={{ width: '18rem' }}>
-                        <Card.Header>Tipo: Fantasma</Card.Header>
-                        <Card.Img width="80" height="100" variant="top" src="https://img.pokemondb.net/sprites/black-white/anim/normal/gengar.gif" className="d-block mx-auto w-50"/>
-                        <Card.Body>
-                            <Card.Title className="text-center">Gengar</Card.Title>
-                            <ListGroup variant="flush">
-                                <ListGroup.Item>Hp: ##</ListGroup.Item>
-                                <ListGroup.Item>Ataque: ##</ListGroup.Item>
-                                <ListGroup.Item>Defensa: ##</ListGroup.Item>
-                                <ListGroup.Item>E.Ataque: ##</ListGroup.Item>
-                                <ListGroup.Item>E.Defensa: ##</ListGroup.Item>
-                                <ListGroup.Item>Velocidad: ##</ListGroup.Item>
-                            </ListGroup>
-                        </Card.Body>
-                        </Card>
-
-                        
+                        {pokemons?.slice(0,151).map((pokemon) => (
+                            <Card className="mx-auto" style={{ width: '18rem' }}>
+                            <Card.Header><b>Tipo:</b> {pokemon.type}</Card.Header>
+                            <Card.Img width="80" height="100" variant="top" src={pokemon.imggif} className="d-block mx-auto w-50"/>
+                            <Card.Body>
+                                <Card.Title className="text-center">{pokemon.id} - {pokemon.name}</Card.Title>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item><b>Hp:</b> {pokemon.hp}</ListGroup.Item>
+                                    <ListGroup.Item><b>Ataque:</b> {pokemon.attack}</ListGroup.Item>
+                                    <ListGroup.Item><b>Defensa:</b> {pokemon.defense}</ListGroup.Item>
+                                    <ListGroup.Item><b>E.Ataque:</b> {pokemon.sp_atk}</ListGroup.Item>
+                                    <ListGroup.Item><b>E.Defensa:</b> {pokemon.sp_def}</ListGroup.Item>
+                                    <ListGroup.Item><b>Velocidad:</b> {pokemon.speed}</ListGroup.Item>
+                                </ListGroup>
+                            </Card.Body>
+                            </Card>  
+                        ))}
                     </div>
                 </div>
             </div>
